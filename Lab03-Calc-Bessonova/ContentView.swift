4753bb4d5a3993ac3b4f6be33a8c0431ed6caf21
Cybergirl01 <sveta-bessonova@list.ru>
2021-12-16 11:33:52 +0300

    Initial Commit

---
+++ Lab03-Calc-Bessonova/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab03-Calc-Bessonova
+//
+//  Created by user on 16.12.2021.
+//  Copyright © 2021 KITMobile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
