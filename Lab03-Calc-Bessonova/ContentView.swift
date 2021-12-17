f3a0fb38e898c57d3d770f724bbc0db1e688bb93
Cybergirl01 <sveta-bessonova@list.ru>
2021-12-17 10:33:31 +0300

    Added more actions for buttons

--- Lab03-Calc-Bessonova/ContentView.swift
+++ Lab03-Calc-Bessonova/ContentView.swift
@@ -16,34 +16,35 @@ struct ContentView: View {
     var body: some View {
 			ZStack{
 				Color("Background")
-				VStack(spacing:1){
+				VStack(spacing: 1){
 					Spacer()
 					Text("0").font(.system(size:80))
 						.frame(width: UIScreen.main.bounds.width, alignment: .trailing)
 						.foregroundColor(.white)
 					HStack(spacing:1){
-						Button(action:{
-							self.a = "0"
+						
+						Button(action:{self.a = "0"
 							self.b = "0"
 							self.fn = ""
-						})
-						{
+						}){
 							
 					ZStack{
 						Color("btn_spec")
 						Text("AC").foregroundColor(Color("col_textbtn"))					}
 					}
-					Button(action:{}){
+						Button(action:{self.fn = "+/-"}){
 					ZStack{
 						Color("btn_spec")
 						Image(systemName: "plusminus").foregroundColor(Color("col_textbtn"))					}
 					}
-					Button(action:{}){
+						Button(action:{self.fn = "%"}){
 					ZStack{
 						Color("btn_spec")
 						Text("%").foregroundColor(Color("col_textbtn"))					}
 					}
-					Button(action:{}){
+						Button(action:{
+							self.fn = "/"
+						}){
 					ZStack{
 						Color("btn_action")
 						Image(systemName: "divide").foregroundColor(Color("col_textbtn"))
@@ -52,46 +53,91 @@ struct ContentView: View {
 					}
 					}.frame(height: UIScreen.main.bounds.width / 4)
 					HStack(spacing: 1){
-						Button(action:{}){
+						Button(action:{if (self.fn.count == 0)
+						{
+							self.a += "7"
+							}
+						else{
+							self.b += "7"
+							}
+							
+						}){
 							ZStack{
 								Color("btn_digit")
 								Text("7").foregroundColor(Color("col_textbtn"))							}
 							}
-							Button(action:{}){
+						Button(action:{ if (self.fn.count == 0){
+							self.a += "8"
+							}
+						else{
+							self.b += "8"
+							}
+							
+							
+						}){
+							
 							ZStack{
 								Color("btn_digit")
 								Text("8").foregroundColor(Color("col_textbtn"))							}
-							}
-							Button(action:{}){
+						}
+						
+					}
+							Button(action:{
+							if (self.fn.count == 0){
+									self.a += "9"
+									}
+								else{
+									self.b += "9"
+									}							}){
 							ZStack{
 								Color("btn_digit")
 								Text("9").foregroundColor(Color("col_textbtn"))							}
 							}
-							Button(action:{}){
+						Button(action:{self.fn = "*"}){
 							ZStack{
 								Color("btn_action")
 								Image(systemName: "multiply").foregroundColor(Color("col_textbtn"))							}
 					}
 				}.frame(height: UIScreen.main.bounds.width / 4)
 					HStack(spacing:1){
-							Button(action:{}){
+							Button(action:{
+								
+								if (self.fn.count == 0){
+									self.a += "4"
+									}
+								else{
+									self.b += "4"
+									}							}){
 								ZStack{
 									Color("btn_digit")
 									Text("4").foregroundColor(Color("col_textbtn"))								}
 								}
-								Button(action:{}){
+								Button(action:{
+									if (self.fn.count == 0){
+										self.a += "5"
+										}
+									else{
+										self.b += "5"
+										}
+								}){
 								ZStack{
 									Color("btn_digit")
 									Text("5").foregroundColor(Color("col_textbtn"))								}
 								}
-								Button(action:{}){
+								Button(action:{
+		if (self.fn.count == 0){
+										self.a += "6"
+										}
+									else{
+										self.b += "6"
+										}								}){
 								ZStack{
 									Color("btn_digit")
 									Text("6")
 										.foregroundColor(Color("col_textbtn"))
 								}
 								}
-								Button(action:{}){
+						Button(action:{self.fn = "-"}){
 								ZStack{
 									Color("btn_action")
 									Text("-").foregroundColor(Color("col_textbtn"))								}
@@ -116,7 +162,7 @@ struct ContentView: View {
 										.foregroundColor(Color("col_textbtn"))
 								}
 								}
-								Button(action:{}){
+					Button(action:{self.fn = "+"}){
 								ZStack{
 									Color("btn_action")
 									Text("+").foregroundColor(Color("col_textbtn"))								}
@@ -141,13 +187,14 @@ struct ContentView: View {
 									Text("=").foregroundColor(Color("col_textbtn"))								}
 						}
 					}.frame(height: UIScreen.main.bounds.width / 4)
-				}.font(.system(size: 25))
 			}
-    }
+	}
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
